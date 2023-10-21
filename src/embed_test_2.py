ba6671bae50bcd4e70419d422dee9c3cbb0bd604
KrulesExGamer <57680132+KrulesExGamer@users.noreply.github.com>
2023-10-21 17:01:50 -0300

    Corrigido

--- src/embed_test_2.py
+++ src/embed_test_2.py
@@ -3,14 +3,32 @@ from MusicArchivist import MusicArchivist
 import os
 from dotenv import load_dotenv
 
+def json_to_text(musics):
+    musics_text = []
+    for music in musics:
+        text = ""
+        for field in music:
+            text += (f"{field}: {music[field]}\n")
+
+    musics_text.append(text)
+
+    return musics_text
+
 # Setup
 load_dotenv()
-api_is_active = True
+api_is_active = False
 token = os.environ.get('COHERE_TOKEN')
-music_file_path = 'data/test.json'
+music_file_path = '../data/musics.json'
 
 # Code 
 archivist = MusicArchivist(music_file_path)
-print(archivist.musics)
+
+music_texts = json_to_text(archivist.musics)
+
+print(music_texts)
 
 api = ApiMaster(token, api_is_active)
+api = api.connect()
+
+embbeds = api.embed(music_texts[0])
+print(embbeds)
